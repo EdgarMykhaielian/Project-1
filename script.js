8364ea3d447ad06607058bb27a6c8827af55c19b
Eddy <edgarmykhaielian@gmail.com>
2023-01-30 20:56:25 +0200

    complete page add screensaver for video section

---
+++ script.js
@@ -0,0 +1,20 @@
+//Show Youtube + hide screensaver
+const youtube = document.querySelector('.video__youtube')
+const screensaver = document.querySelector('.video__screensaver')
+const videoBtn = document.querySelector('.video__btn')
+videoBtn.addEventListener('click',showYoutube)
+
+function showYoutube() {
+    youtube.style.display='block'
+    screensaver.style.display='none'
+}
+
+
+//Change Theme
+// const logo = document.querySelector('.union')
+
+// logo.addEventListener('click',changeTheme)
+
+// function changeTheme(){
+    
+// }
