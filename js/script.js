ce6462f890002a3e4223104eeba548a4554295e7
Eddy <edgarmykhaielian@gmail.com>
2023-02-04 13:11:41 +0200

    add burger and basic responsive

--- js/script.js
+++ js/script.js
@@ -2,9 +2,34 @@
 const youtube = document.querySelector('.video__youtube')
 const screensaver = document.querySelector('.video__screensaver')
 const videoBtn = document.querySelector('.video__btn')
-videoBtn.addEventListener('click',showYoutube)
+videoBtn.addEventListener('click', showYoutube)
 
 function showYoutube() {
-    youtube.style.display='block'
-    screensaver.style.display='none'
+    youtube.style.display = 'block'
+    screensaver.style.display = 'none'
+}
+
+//BURGER MENU
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const hero = document.querySelector(".hero__content")
+
+hamburger.addEventListener("click", burgerMenu);
+
+function burgerMenu() {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+    hero.classList.toggle("hidden")
+}
+
+// hamburger menu - close when we click a link.
+
+const menuItem = document.querySelectorAll(".menu__item");
+menuItem.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+    hero.classList.toggle("hidden")
 }
